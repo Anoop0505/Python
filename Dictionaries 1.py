2eef5ffd526382bc8c8736f4aa6d35273ac32eec
Anoop0505 <56749231+Anoop0505@users.noreply.github.com>
2019-10-21 15:34:10 +0530

    Add files via upload

---
+++ Dictionaries 1.py
@@ -0,0 +1,76 @@
+#item={"Biscuit":10,
+#     "BREAD":20,
+#     "SUGER":84,
+#     "JAM": 40,
+#     "CHEESE": 130,
+#     "BOURNVITA":240,
+#     "TEAPowder": 200,
+#     "MILK": 30,
+#     "COFFEEPowder": 40,
+#     "CORNFLAKES": 56,
+#     "RICE": 78,
+#     "DAL": 100,
+#     }
+#print(item)
+#lf=input("enter the item").split(" ")
+#print(type(lf))
+#totalcost=0
+#for i in lf:
+#    if i in item:
+#        totalcost=totalcost+item[i]
+#print(totalcost)
+
+
+
+#day={}
+#n=int(input('Enter n'))
+#for i in range(0,n):
+#    a=input('Enter the DAY:\n')
+#    b=input('Enter the temperature:\n')
+#    day[a]=b
+#print(day)
+#c=input('Enter the day')
+#for i in day:
+#    if i in day:
+#        v=day[i]
+#print(v)
+
+#day={}
+#n=int(input('Enter n'))
+#for i in range(0,n):
+#    a=int(input('Enter the number:\n'))
+#    b=a*a
+#    day[a]=b
+#print(day)
+
+
+
+
+
+
+
+
+
+#graph={'PC1':['PC2','PC4'],
+#       'PC2':['PC1','PC3','PC5'],
+#       'PC3':['PC2','PC5'],
+#       'PC4':['PC1','PC5'],
+#       'PC5':['PC2','PC3','PC4']
+#       }
+#print(graph)
+#print("First")
+#first=graph['PC2']
+#print(first)
+#second=[]
+#for i in first:
+#    test=graph[i]
+#    for j in test:
+#        if(j not in second and j!='PC2' and j not in first):
+#            second.append(j)
+#           
+#print("Second neighbors")
+#print(second)
+
+
+
+
